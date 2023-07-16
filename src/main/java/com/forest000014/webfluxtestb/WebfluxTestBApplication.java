507f7ba13061d8cb620b51c02939fe1cabbe3228
forest000014 <forest000014@gmail.com>
2023-07-16 20:57:00 +0900

    first commit

---
+++ src/main/java/com/forest000014/webfluxtestb/WebfluxTestBApplication.java
@@ -0,0 +1,13 @@
+package com.forest000014.webfluxtestb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxTestBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxTestBApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/forest000014/webfluxtestb/WebfluxTestBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forest000014.webfluxtestb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxTestBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
