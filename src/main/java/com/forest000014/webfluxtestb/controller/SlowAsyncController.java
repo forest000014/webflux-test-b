557f78696e72f0052c72144738c43dcdbaa0a24e
forest000014 <forest000014@gmail.com>
2023-07-16 21:40:55 +0900

    Server A 호출 없이, 5초간 sleep만 하고 응답 내려주는 /sleep API

--- src/main/java/com/forest000014/webfluxtestb/controller/SlowAsyncController.java
+++ src/main/java/com/forest000014/webfluxtestb/controller/SlowAsyncController.java
@@ -10,6 +10,7 @@ import reactor.core.publisher.Mono;
 public class SlowAsyncController {
 
     private final String SLOW_SERVER_URL = "http://localhost:8081/slow?name=david";
+    private final long SLEEP_TIME_SECONDS = 5;
 
     @GetMapping("/slow")
     @ResponseBody
@@ -22,5 +23,18 @@ public class SlowAsyncController {
                 .bodyToMono(String.class);
 
         return bodyMono;
+    }
+
+    @GetMapping("/sleep")
+    @ResponseBody
+    public Mono<String> sleepMethod() throws InterruptedException {
+        System.out.println("====== /sleep requested !! ======");
+
+        for (int i = 1; i <= SLEEP_TIME_SECONDS; i++) {
+            Thread.sleep(1000);
+            System.out.printf("sleeping for %d second(s)...\n", i);
+        }
+
+        return Mono.just("Slept for 5 seconds.");
     }
 }
